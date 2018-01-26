de19e9c2b30b769a9aff2079be25bf06da0485e0
heekei <heekei@foxmail.com>
2018-01-26 18:10:25 +0800

    fix bug

--- extension.js
+++ extension.js
@@ -6,43 +6,50 @@ let fs = require("fs");
 function activate(context) {
     let codeReplace = vscode.commands.registerCommand("extension.codeReplace", function () {
         let editor = vscode.window.activeTextEditor;
-        if (!editor) { return; }
+        if (!editor) {
+            return;
+        }
         // let fname = editor.document.fileName.substring(0, editor.document.fileName.lastIndexOf("."));
         let ename = editor.document.fileName.substring(editor.document.fileName.lastIndexOf("."), editor.document.fileName.length);
         // let getConfig = vscode.workspace.getConfiguration;
         // let config = getConfig("codeReplace");
         // let templates = config.templates;
-        let res = editor.document.getText().match(/\{\{([^}}]+)?\}\}/g);//["{{key}}","{{key1}}"]
+        let res = editor.document.getText().match(/\{\{([^}}]+)?\}\}/g); //["{{key}}","{{key1}}"]
         // let reg = new RegExp(/\{\{(\w+?)\}\}/g);
 
+        let errorArr = [];
+        editor.edit(function (builder) {
+            res.map(function (value) {
+                let text = editor.document.getText();
+                let prop = value.replace(/[\{|\}]/g, ""); //key
+                let start = editor.document.positionAt(text.indexOf(value));
+                let end = editor.document.positionAt(text.indexOf(value) + value.length);
+                let range = new vscode.Range(start, end);
+                try {
+                    let filestr = fs.readFileSync(vscode.workspace.rootPath + "/ctr-tmps/" + prop + ename);
+                    builder.replace(range, filestr.toString());
+                } catch (error) {
+                    errorArr.push(prop);
+                }
+            })
+        }).then(function (res) {
+            console.log(res, errorArr);
+        });
+        // res.map(function (value) {
+        //     let text = editor.document.getText();
+        //     let prop = value.replace(/[\{|\}]/g, ""); //key
+        //     let start = editor.document.positionAt(text.indexOf(value));
+        //     let end = editor.document.positionAt(text.indexOf(value) + value.length);
+        //     let range = new vscode.Range(start, end);
+        //     // var data = fs.readFileSync(vscode.workspace.rootPath + "/templates/" + prop + ename)
+        //     // console.log(data)
 
-
-        res.map(function (value) {
-            let text = editor.document.getText();
-            let prop = value.replace(/[\{|\}]/g, "");//key
-            let start = editor.document.positionAt(text.indexOf(value));
-            let end = editor.document.positionAt(text.indexOf(value) + value.length);
-            let range = new vscode.Range(start, end);
-            // var data = fs.readFileSync(vscode.workspace.rootPath + "/templates/" + prop + ename)
-            // console.log(data)
-            fs.readFile(vscode.workspace.rootPath + "/templates/" + prop + ename, function (err, data) {
-                if (err) { return; }
-                editor.edit(function (builder) {
-                    builder.replace(range, data.toString());
-                }).then(function (b) {
-                    console.log(prop + " " + b)
-                })
-            });
-
-            // var data = fs.readFileSync(vscode.workspace.rootPath + "/templates/" + prop + ename);
-            // edit.replace(editor.document.uri, range, data.toString());
-            // vscode.workspace.applyEdit(edit)
-        })
+        //     let filestr = fs.readFileSync(vscode.workspace.rootPath + "/ctr-tmps/" + prop + ename);
+        // })
     })
     context.subscriptions.push(codeReplace);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
-function deactivate() {
-}
+function deactivate() {}
 exports.deactivate = deactivate;
