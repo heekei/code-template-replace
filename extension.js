97cb3ff60e4534c92d3f4a14009e0656010500c0
Heekei <heekei@foxmail.com>
2018-12-01 11:57:40 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 let vscode = require('vscode');
 let fs = require("fs");
 
@@ -7,11 +5,7 @@ function activate(context) {
     let codeReplace = vscode.commands.registerCommand("extension.codeReplace", function () {
         let editor = vscode.window.activeTextEditor;
         if (!editor) return;
-        // let fname = editor.document.fileName.substring(0, editor.document.fileName.lastIndexOf("."));
         let ename = editor.document.fileName.substring(editor.document.fileName.lastIndexOf("."), editor.document.fileName.length);
-        // let getConfig = vscode.workspace.getConfiguration;
-        // let config = getConfig("codeReplace");
-        // let templates = config.templates;
         let res = editor.document.getText().match(/\{\{(((?!}}).)+)\}\}/g); //["{{key}}","{{key1}}"]
 
         let errorArr = [];
@@ -23,7 +17,7 @@ function activate(context) {
                 let end = editor.document.positionAt(text.indexOf(value) + value.length);
                 let range = new vscode.Range(start, end);
                 try {
-                    let filestr = fs.readFileSync(vscode.workspace.rootPath + "/ctr-tmps/" + prop + ename);
+                    let filestr = fs.readFileSync(`${vscode.workspace.rootPath}/ctr-tmps/${prop}${ename}`);
                     builder.replace(range, filestr.toString());
                 } catch (error) {
                     errorArr.push(prop);
@@ -36,6 +30,5 @@ function activate(context) {
     context.subscriptions.push(codeReplace);
 }
 exports.activate = activate;
-// this method is called when your extension is deactivated
 function deactivate() {}
 exports.deactivate = deactivate;
